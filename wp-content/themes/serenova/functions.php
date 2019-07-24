94c3cbcb9716f078b13e68e1ef7918155c116892
Justin Young <justin@emersonstone.com>
2019-07-24 16:59:10 -0600

    remove gutenberg files

--- wp-content/themes/serenova/functions.php
+++ wp-content/themes/serenova/functions.php
@@ -12,6 +12,17 @@ add_filter( 'timber/cache/mode', function () {
     return 'none';
 } );
 
+// Disable Gutenberg editor.
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+// Don't load Gutenberg-related stylesheets.
+add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
+function remove_block_css() {
+    wp_dequeue_style( 'wp-block-library' ); // Wordpress core
+    wp_dequeue_style( 'wp-block-library-theme' ); // Wordpress core
+    wp_dequeue_style( 'wc-block-style' ); // WooCommerce
+    wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront theme
+}
+
 // if ( class_exists( 'Timber' ) ){
 //     Timber::$cache = true;
 // }
